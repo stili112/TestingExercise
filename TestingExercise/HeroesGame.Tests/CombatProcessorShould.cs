00986b4b4ee84dfe88cbb733bd4da16ad80b1f90
stili112 <stili16@abv.bg>
2020-02-09 12:46:23 +0200

    Initial Commit

---
+++ TestingExercise/HeroesGame.Tests/CombatProcessorShould.cs
@@ -0,0 +1,41 @@
+﻿using HeroesGame.Contract;
+using HeroesGame.Implementation.Hero;
+using HeroesGame.Implementation.Monster;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeroesGame.Tests
+{
+    public class CombatProcessorShould
+    {
+        private CombatProcessor _cp;
+
+        [SetUp]
+        public void Setup()
+        {
+            _cp = new CombatProcessor(new Hunter());
+        }
+
+        [Test]
+        public void InitializeCorrectly()
+        {
+            Assert.That(_cp.Hero, Is.Not.Null);
+            Assert.That(_cp.Logger, Is.Not.Null);
+            Assert.That(_cp.Logger, Is.Empty);
+        }
+
+        [Test]
+        public void FightCorrectlyAndRepeatedly_StrongEnemy()
+        {
+            IMonster monster = new MedusaTheGorgon(50);
+
+            _cp.Fight(monster);
+            var logger = _cp.Logger;
+
+            Assert.That(logger, Has.Count.EqualTo(12));
+            Assert.That(logger, Does.Contain("The hero dies on level 1 after 4 fights."));
+        }
+    }
+}
---
+++ TestingExercise/HeroesGame.Tests/HeroShould.cs
@@ -0,0 +1,96 @@
+﻿using HeroesGame.Constant;
+using HeroesGame.Contract;
+using HeroesGame.Implementation.Hero;
+using Moq;
+using Moq.Protected;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeroesGame.Tests
+{
+    public class HeroShould
+    {
+        //private IHero _hero;
+        private Mock<Mage> _hero;
+
+        /*[SetUp]
+        public void Setup()
+        {
+            this._hero = new Mage();
+        }*/
+
+        [SetUp]
+        public void Setup()
+        {
+            this._hero = new Mock<Mage>();
+            this._hero.Protected().Setup("LevelUp").CallBase();
+        }
+
+        [Test]
+        public void HaveCorrectInitialValues()
+        {
+            Assert.That(_hero.Object.Level, Is.EqualTo(HeroConstants.InitialLevel), "Level not expected");
+            Assert.That(_hero.Object.Experience, Is.EqualTo(HeroConstants.InitialExperience));
+            Assert.That(_hero.Object.MaxHealth, Is.EqualTo(HeroConstants.InitialMaxHealth));
+            Assert.That(_hero.Object.Health, Is.EqualTo(HeroConstants.InitialMaxHealth));
+            Assert.That(_hero.Object.Armor, Is.EqualTo(HeroConstants.InitialArmor));
+            Assert.That(_hero.Object.Weapon, Is.Not.Null);
+        }
+
+        [Test]
+        public void TakeHitCorrectly()
+        {
+            //Act
+            var damage = 50;
+            _hero.Object.TakeHit(damage);
+
+            //Assert
+            Assert.That(_hero.Object.Health, Is.EqualTo(HeroConstants.InitialMaxHealth - damage + HeroConstants.InitialArmor));
+        }
+
+        [Test]
+        [TestCase(10)]
+        [TestCase(20)]
+        [TestCase(30)]
+        public void TakeHitCorrectly_TestCase(int damage)
+        {
+            //Act
+            _hero.Object.TakeHit(damage);
+
+            //Assert
+            Assert.That(_hero.Object.Health, Is.EqualTo(HeroConstants.InitialMaxHealth - damage + HeroConstants.InitialArmor));
+        }
+
+        [Test]
+        public void TakeHitCorrectly_Combinatorial([Values(40,50,60)] int damage)
+        {
+            //Act
+            _hero.Object.TakeHit(damage);
+
+            //Assert
+            Assert.That(_hero.Object.Health, Is.EqualTo(HeroConstants.InitialMaxHealth - damage + HeroConstants.InitialArmor));
+        }
+
+        [Test]
+        public void TakeHitCorrectly_Range([Range(70, 100, 10)] int damage)
+        {
+            //Act
+            _hero.Object.TakeHit(damage);
+
+            //Assert
+            Assert.That(_hero.Object.Health, Is.EqualTo(HeroConstants.InitialMaxHealth - damage + HeroConstants.InitialArmor));
+        }
+
+        [Test]
+        public void ThrowExceptionForNegativeTakeHitValue()
+        {
+            //Act
+            var damage = -50;
+
+            //Assert
+            Assert.Throws<ArgumentException>(() => _hero.Object.TakeHit(damage), "Damage value cannot be negative");
+        }
+    }
+}
